37d5e9cb948fd119ac6b992c0c4b7622f0ac5a12
Ion Cretu <cretuion94@gmail.com>
2020-07-07 02:07:39 +0300

    v0.1.0

--- app.js
+++ app.js
@@ -1,28 +1,52 @@
-  
 const express = require('express');
+const morgan = require('morgan');
+const mongoose = require('mongoose');
+const blogRoutes = require('./routes/blogRoutes');
 
 // express app
 const app = express();
+const port = 5000;
+// connect to mongodb & listen for requests
+const dbURI = "mongodb://localhost:27017/?readPreference=primary&appname=MongoDB%20Compass%20Community&ssl=false";
 
-// listen for requests
-app.listen(3000);
+mongoose.connect(dbURI, { useNewUrlParser: true, useUnifiedTopology: true })
+  .then(result => app.listen(port, () => console.log(`Blog app listening at http://localhost:${port}`)))
+  .catch(err => console.log(err));
+
+// register view engine
+app.set('view engine', 'ejs');
+
+// middleware & static files
+app.use(express.static('public'));
+app.use(express.urlencoded({ extended: true }));
+app.use(morgan('dev'));
+app.use((req, res, next) => {
+  res.locals.path = req.path;
+  next();
+});
 
 app.get('/', (req, res) => {
-  // res.send('<p>home page</p>');
-  res.sendFile('./views/index.html', { root: __dirname });
+  const blogs = [
+    {title: 'Yoshi finds eggs', snippet: 'Lorem ipsum dolor sit amet consectetur'},
+    {title: 'Mario finds stars', snippet: 'Lorem ipsum dolor sit amet consectetur'},
+    {title: 'How to defeat bowser', snippet: 'Lorem ipsum dolor sit amet consectetur'},
+  ];
+  res.render('index', { title: 'Home', blogs });
+});
+
+// routes
+app.get('/', (req, res) => {
+  res.redirect('/blogs');
 });
 
 app.get('/about', (req, res) => {
-  // res.send('<p>about page</p>');
-  res.sendFile('./views/about.html', { root: __dirname });
+  res.render('about', { title: 'About' });
 });
 
-// redirects
-app.get('/about-us', (req, res) => {
-  res.redirect('/about');
-});
+// blog routes
+app.use('/blogs', blogRoutes);
 
 // 404 page
 app.use((req, res) => {
-  res.status(404).sendFile('./views/404.html', { root: __dirname });
+  res.status(404).render('404', { title: '404' });
 });
